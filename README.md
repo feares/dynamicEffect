68bd7c45473d8fe92dd3cad8a72253e54a70fc1d
windwithfo <434503826@qq.com>
2015-09-01 16:52:33 +0800

    添加文档和 init 示例

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ ea.css 和 ea.js 已做了压缩
 源码在 ea_src.css 和 ea_src.js
 amd 源码为：ea_src_amd.js
 
-因暂时未编写文档，使用方法请参考 demo 调用或自行阅读源码获取
+文档位置：doc/index.html，放到webserver下即可查看
---
+++ doc/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "EA"
+    ],
+    "modules": [
+        "EA"
+    ],
+    "allModules": [
+        {
+            "displayName": "EA",
+            "name": "EA",
+            "description": "EA is easy animation",
+            "classes": [
+                {
+                    "name": "EA"
+                }
+            ]
+        }
+    ]
+} };
+});
